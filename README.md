576a7309370ce0f6da842b2bcb1f0a55ddea3ebf
Pat Lowe <lowe-patrick@att.net>
2023-08-02 19:21:04 -0700

    added function to generate svg file but there are bugs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SVG-Logo-Generator
+
+## https://www.w3schools.com/graphics/svg_rect.asp
--- index.js
+++ index.js
@@ -1,32 +1,58 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Square, Circle, Triangle } = require("./lib/Shapes.js");
 
-function userprompt {
-    inquirer.prompt([
+function writetofile(filename, data) {
+  let SVG = '<svg width="300" height="" xmlns="http://www.w3.org/2000/svg">';
+  let shapechoice;
+  if (data.shape === "Square") {
+    shapechoice = new Square();
+  } else if (data.shape === "Circle") {
+    shapechoice = new Circle();
+  } else if (data.shape === "Triangle") {
+    shapechoice = new Triangle();
+  }
+  SVG += `${data.shape}`;
+  SVG += `${data.shapecolor}`;
+  SVG += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${data.textcolor}">${data.text}</text>`;
 
-        {
-            type: 'list',
-            message: 'What shape would like your logo to be?',
-            choices: ['Square', 'Circle', 'Triangle'],
-            name: 'shape'
-        },
-        {
-            type: 'input',
-            message: 'What color would you like your logo to be? (Please enter a color name or a hex color code)',
-            name: 'shapecolor'
-        },
-        {
-            type: 'input',
-            message: 'What text would you like your logo to say? (3 letters maximum)',
-            name: 'text'
-        },
-        {
-            type: 'input',
-            message: 'What color would you like your text to be?(Please enter a color name or a hex color code)',
-            name: 'textcolor'
-        },
-
-
-    ])
-
+  fs.writeFile(filename, SVG, (err) => {
+    if (err) throw err;
+    console.log("generated SVG logo!");
+  });
 }
+
+function userprompt() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What shape would like your logo to be?",
+        choices: ["Square", "Circle", "Triangle"],
+        name: "shape",
+      },
+      {
+        type: "input",
+        message:
+          "What color would you like your logo to be? (Please enter a color name or a hex color code)",
+        name: "shapecolor",
+      },
+      {
+        type: "input",
+        message:
+          "What text would you like your logo to say? (3 letters maximum)",
+        name: "text",
+      },
+      {
+        type: "input",
+        message:
+          "What color would you like your text to be?(Please enter a color name or a hex color code)",
+        name: "textcolor",
+      },
+    ])
+    .then((data) => {
+      writetofile("logo.svg", data);
+    });
+}
+
+userprompt();
